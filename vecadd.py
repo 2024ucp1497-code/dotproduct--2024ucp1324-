d9d4a3d60ef0043093db0d0c7daf4e2532457b99
Sanjeev Yadav <sy4aug2004@gmail.com>
2025-11-13 12:28:17 +0530

    first change

---
+++ vecadd.py
@@ -0,0 +1,20 @@
+from multiprocessing import Pool
+
+v1 = list(map(int, input("enter first vector:").split()))
+v2 = list(map(int, input("enter second vector:").split()))
+
+if len(v1) != len(v2):
+                 print("Error")
+                 
+else :
+
+      
+      print("dot product result= ",sum(x*y for x,y in zip(v1,v2))) 
+
+      with Pool() as pool:
+      	result = pool.map(multiply,zip(v1,v2))
+      print("parallelism dot product= ",p)
+
+   	
+   	
+
